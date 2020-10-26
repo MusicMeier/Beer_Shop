fe9f1ceb1af6656f1fddef402cdc6d475a16ab34
Music <musicm987@gmail.com>
2020-10-26 16:38:19 -0600

    added methods and html to create error messages

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -5,9 +5,23 @@ class BeersController < ApplicationController
     end
 
     def create
+        @beer = Beer.new(beer_params)
+
+        if (@beer.save)
+            redirect_to beers_path
+        else
+            render :new
+        end
+
     end
 
     def new
         @beer = Beer.new
+    end
+
+    private
+
+    def beer_params 
+        params.require(:beer).permit(:name, :abv)
     end
 end
